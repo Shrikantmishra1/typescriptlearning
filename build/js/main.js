92e7c0a8f4b3cfd1b7e307135f7cde2b3fc7ce96
shrikant mishra <mishrashri2001@gmail.com>
2024-01-09 20:38:48 +0530

    learning_09_1_24_3

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,43 @@
 "use strict";
-let a = 12;
-let b = '6';
-let c = 2;
-console.log(b);
-console.log(c * b);
+// let a:number=12
+// let b:string='6'
+// let c:number=2;
+// console.log(b);
+// console.log(c+b);
+//data typed in typescript 
+// let myName:string;
+// let meaningOfLife:number;
+// let isLoading:boolean;
+// let album:any;//any type
+// let cm:string | number;//union type
+// cm='fdsfsd';
+// cm=12
+// album='1323';
+// album=1231
+// album=true
+// myName='NAme';
+// myName='jhone';
+// meaningOfLife=21
+// isLoading=true
+// console.log(myName);
+// const sum=(a:number,b:string)=>{
+//     return a+b;
+// }
+// console.log(sum(12,'ddad'));
+// let re:RegExp=/\w+/g //RegExp type in typescript 
+//:TODO: checklison-3
+let stringArr = ['name', 'hey', 'ru'];
+let guitars = ['Strat', 'Les paul', 5150];
+let mixedData = ['EVH', 1984, true];
+stringArr[0] = 'amfd';
+stringArr.push('sfs');
+mixedData[0] = 'abstr';
+//add ing data in array
+let test = [];
+let mono = [];
+//Tuple
+let myTuple = ['Dave', 42, true];
+let mixed = ['Johne', 1, true];
+mixed = myTuple;
+console.log(mixed);
+console.log(myTuple);
