187160305e4c53fc6f7584e17719f822f094a1dd
shrikant mishra <mishrashri2001@gmail.com>
2024-01-10 15:14:02 +0530

    lesson_3_2

--- build/js/main.js
+++ build/js/main.js
@@ -2,65 +2,25 @@
 // let a:number=12
 // let b:string='6'
 // let c:number=2;
-//Literal Types
-let myName;
-let username;
-username = "Amy";
-//functions
-const add = (a, b) => {
-    return a + b;
+//convert to more or less specific 
+let a = 'hello';
+let b = a; //less specific
+let c = a; //more specific
+let d = 'World';
+let e = 'World';
+const addOrConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b;
 };
-const logMesg = (message) => {
-    console.log(message);
-};
-logMesg("Helo");
-logMesg(add(2, 4));
-let subtract = function (c, d) {
-    return c - d;
-};
-let multiply = function (c, d) {
-    return c * d;
-};
-logMesg(multiply(2, 5));
-// OPTIONAL PARAMETERS FOR TYPESCRIPT:
-const addAll = (a, b, c) => {
-    if (typeof c !== 'undefined') {
-        return a + b + c;
-    }
-    return a + b;
-};
-//default parameters
-const sumAll = (a, b, c = 2) => {
-    return a + b + c;
-};
-logMesg(sumAll(2, 4, 9));
-//REST PARAMETERS
-const total = (...nums) => {
-    return nums.reduce((prev, curr) => prev + curr);
-};
-logMesg(total(1, 3, 4, 6));
-//NEVER TYPE
-const createError = (errMsg) => {
-    throw new Error(errMsg);
-};
-const inifinite = () => {
-    let i = 1;
-    while (true) {
-        i++;
-        if (1 > 100)
-            break;
-    }
-};
-//custom function gaurd function
-const isNumber = (value) => {
-    return typeof value === 'number'
-        ? true : false;
-};
-//use of never type
-const numberORstring = (value) => {
-    if (typeof value === 'string')
-        return 'string';
-    if (typeof value === 'number')
-        return 'number';
-    return createError('This should never happen !');
-};
+let myVal = addOrConcat(2, 2, 'concat');
+//Be caeful TS sees no problem but a string is returned
+let nextval = addOrConcat(2, 2, 'concat');
+//10 as string
+10; //forecasting 
+//THE DOM
+const img = document.querySelector('img');
+const myImg = document.getElementById('#img');
+const nextImg = document.getElementById('nextImg');
+img.src;
+myImg.src;
--- src/main.ts
+++ src/main.ts
@@ -85,75 +85,103 @@
 ///
 // FOR LEARNING FUNCTION BEHAVIOUR IN TYPESCRIPT:
 //Type allias for typescript type
-type stringorNumber = string | number;
-type stringorNumberArray = (string | number)[];
+// type stringorNumber = string | number;
+// type stringorNumberArray = (string | number)[];
 
-type Guitarist = {
-  name?: string;
-  active: boolean;
-  albums: stringorNumberArray;
-};
-type UserId = stringorNumber;
-//Literal Types
-let myName: "Dave";
-let username: "Dave" | "Johne" | "Amy";
-username = "Amy";
-//functions
-const add = (a: number, b: number): number => {
-  return a + b;
-};
-const logMesg = (message: any): void => {
-  console.log(message);
-};
-logMesg("Helo");
-logMesg(add(2, 4));
-let subtract = function (c: number, d: number): number {
-  return c - d;
-};
-//type mathFunction =(a: number , b:number)=>number
-interface mathFunction {(a: number , b:number):number}
-let multiply:mathFunction=function(c,d){
-         return c*d
-}
-logMesg(multiply(2,5));
- // OPTIONAL PARAMETERS FOR TYPESCRIPT:
+// type Guitarist = {
+//   name?: string;
+//   active: boolean;
+//   albums: stringorNumberArray;
+// };
+// type UserId = stringorNumber;
+// //Literal Types
+// let myName: "Dave";
+// let username: "Dave" | "Johne" | "Amy";
+// username = "Amy";
+// //functions
+// const add = (a: number, b: number): number => {
+//   return a + b;
+// };
+// const logMesg = (message: any): void => {
+//   console.log(message);
+// };
+// logMesg("Helo");
+// logMesg(add(2, 4));
+// let subtract = function (c: number, d: number): number {
+//   return c - d;
+// };
+// //type mathFunction =(a: number , b:number)=>number
+// interface mathFunction {(a: number , b:number):number}
+// let multiply:mathFunction=function(c,d){
+//          return c*d
+// }
+// logMesg(multiply(2,5));
+//  // OPTIONAL PARAMETERS FOR TYPESCRIPT:
        
- const addAll=(a:number,b:number, c?:number):number=>{
-        if(typeof c!=='undefined'){
-               return a+b+c
-        }
-        return a+b
- }
- //default parameters
- const sumAll=(a:number,b:number, c:number=2):number=>{
+//  const addAll=(a:number,b:number, c?:number):number=>{
+//         if(typeof c!=='undefined'){
+//                return a+b+c
+//         }
+//         return a+b
+//  }
+//  //default parameters
+//  const sumAll=(a:number,b:number, c:number=2):number=>{
         
-        return a+b+c
- }
- logMesg(sumAll(2,4,9))
- //REST PARAMETERS
- const total=(...nums:number[]):number=>{
-        return nums.reduce((prev,curr)=>prev+curr)
- }
-logMesg(total(1,3,4,6))
-//NEVER TYPE
-const createError=(errMsg:string)=>{
-         throw new Error(errMsg)
+//         return a+b+c
+//  }
+//  logMesg(sumAll(2,4,9))
+//  //REST PARAMETERS
+//  const total=(...nums:number[]):number=>{
+//         return nums.reduce((prev,curr)=>prev+curr)
+//  }
+// logMesg(total(1,3,4,6))
+// //NEVER TYPE
+// const createError=(errMsg:string)=>{
+//          throw new Error(errMsg)
+// }
+// const inifinite=()=>{
+//          let i:number=1
+//          while(true){
+//                i++
+//                if(1>100)break
+//          }
+// }
+// //custom function gaurd function
+// const isNumber =(value:any):boolean=>{
+//          return typeof value === 'number'
+//          ?true :false
+// }
+// //use of never type
+// const numberORstring=(value:number | string):string =>{
+//          if(typeof value==='string') return 'string'
+//          if(typeof value==='number') return 'number'
+//          return createError('This should never happen !')
+// }
+type One=string
+type Tw=string | number
+type Three='hello'
+
+//convert to more or less specific 
+let a:One='hello'
+let b=a as Tw //less specific
+let c=a as Three //more specific
+let d=<One>'World'
+let e=< string | number>'World'
+const addOrConcat=(a:number, b:number, c:'add'|'concat'):number |string =>{
+          if (c==='add') return a+b
+          return '' +a + b
 }
-const inifinite=()=>{
-         let i:number=1
-         while(true){
-               i++
-               if(1>100)break
-         }
-}
-//custom function gaurd function
-const isNumber =(value:any):boolean=>{
-         return typeof value === 'number'
-         ?true :false
-}
-//use of never type
-const numberORstring=(value:number | string):string =>{
-         if(typeof value==='string') return 'string'
-         if(typeof value==='number') return 'number'
-         return createError('This should never happen !')
-}
+let myVal:string =addOrConcat(2,2,'concat') as string
+
+//Be caeful TS sees no problem but a string is returned
+
+let nextval:number =addOrConcat(2,2,'concat') as number
+
+//10 as string
+(10 as unknown) as string //forecasting 
+//THE DOM
+const img=document.querySelector('img')!
+const myImg=document.getElementById('#img') as HTMLImageElement
+const nextImg=<HTMLImageElement> document.getElementById('nextImg') as HTMLImageElement
+img.src
+myImg.src
